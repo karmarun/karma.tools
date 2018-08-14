65303fc043a906fed5acadba0323817510d9e050
Till Hagger <till.hagger@dudagroup.com>
2018-08-14 16:44:25 +0200

    Use recursion instead of loop

--- packages/editor-client/src/api/filter.ts
+++ packages/editor-client/src/api/filter.ts
@@ -1,5 +1,13 @@
-import {expression as e, func as f, Expression} from '@karma.run/sdk'
-import {Condition, ValuePathSegmentType, ConditionType} from '@karma.run/editor-common'
+import {expression as e, data as d, func as f, Expression} from '@karma.run/sdk'
+
+import {
+  Condition,
+  ValuePathSegmentType,
+  ConditionType,
+  ValuePath,
+  ValuePathSegment,
+  ListPathSegment
+} from '@karma.run/editor-common'
 
 export function conditionExpression(value: Expression, condition: Condition): Expression {
   switch (condition.type) {
@@ -11,36 +19,43 @@ export function conditionExpression(value: Expression, condition: Condition): Ex
   }
 }
 
-export function filterExpression(expression: Expression, condition: Condition): Expression {
-  return condition.path.reduce((expression, pathSegment, index) => {
-    const isLast = index === condition.path.length - 1
+export function pathExpression(expression: Expression, pathSegment: ValuePathSegment): Expression {
+  switch (pathSegment.type) {
+    case ValuePathSegmentType.Struct:
+      return e.field(pathSegment.key, expression)
 
-    switch (pathSegment.type) {
-      case ValuePathSegmentType.Struct:
-        return isLast
-          ? conditionExpression(e.field(pathSegment.key, expression), condition)
-          : e.field(pathSegment.key, expression)
+    case ValuePathSegmentType.List:
+      return e.rightFoldList(
+        expression,
+        e.bool(false),
+        f.function(['aggregator', 'value'], e.or(e.scope('aggregator'), expression))
+      )
 
-      case ValuePathSegmentType.List:
-        return e.reduceList(
-          expression,
-          e.bool(false),
-          f.function(
-            ['value', 'nextValue'],
-            e.or(
-              e.scope('value'),
-              isLast ? conditionExpression(e.scope('nextValue'), condition) : expression
-            )
-          )
-        )
+    case ValuePathSegmentType.Optional:
+      return e.if(e.isPresent(expression), expression, e.bool(false))
 
-      case ValuePathSegmentType.Optional:
-        return isLast
-          ? conditionExpression(expression, condition)
-          : e.if(e.isPresent(expression), expression, e.bool(false))
-
-      default:
-        return e.bool(false)
-    }
-  }, expression)
+    default:
+      return e.bool(false)
+  }
 }
+
+export function filterExpression(expression: Expression, condition: Condition): Expression {
+  function recurse(expression: Expression, path: ValuePath): Expression {
+    if (path.length === 0) return expression
+    return recurse(pathExpression(expression, path[path.length - 1]), path.slice(0, -1))
+  }
+
+  return recurse(conditionExpression(expression, condition), condition.path)
+}
+
+console.log(
+  JSON.stringify(
+    filterExpression(e.data(d.list(d.string('1234'), d.string('4321'))), {
+      path: [ListPathSegment()],
+      type: ConditionType.StringEqual,
+      value: '1234'
+    }),
+    undefined,
+    2
+  )
+)
